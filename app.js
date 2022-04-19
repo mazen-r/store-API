5963e41a74cf343cb11221f4fd8233a132149715
Mazen Ramadan <mazenramadan00@gmail.com>
2022-04-19 15:36:17 +0200

    connect to mongodb

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect')
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1>')
+});
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 8000
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
