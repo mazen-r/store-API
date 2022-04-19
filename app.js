e4b83a5df37e55af96f20ef13ee56f45f795ff5a
Mazen Ramadan <mazenramadan00@gmail.com>
2022-04-19 16:04:59 +0200

    products routes

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect')
-
+const productsRouter = require('./routes/products')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 
+//routes
+
 app.get('/', (req, res) => {
-    res.send('<h1>Store API</h1>')
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 });
+
+app.use('/api/v1/products', productsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
