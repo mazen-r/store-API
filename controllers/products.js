59ee872f40620399c34ab44e7fa70d8a169ab8b4
Mazen Ramadan <mazenramadan00@gmail.com>
2022-04-20 00:02:27 +0200

    more queries stuff

--- controllers/products.js
+++ controllers/products.js
@@ -8,25 +8,64 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-    const  { featured, company, name } = req.query
-    const queryObject = {}
-
+    const { featured, company, name, sort, fields, numericFilters } = req.query;
+    const queryObject = {};
+  
     if (featured) {
-        queryObject.featured = featured === 'true' ? true : false
-    } 
+      queryObject.featured = featured === 'true' ? true : false;
+    }
     if (company) {
-        queryObject.company = company
+      queryObject.company = company;
     }
     if (name) {
-        queryObject.name = name
+      queryObject.name = { $regex: name, $options: 'i' };
     }
+    if (numericFilters) {
+      const operatorMap = {
+        '>': '$gt',
+        '>=': '$gte',
+        '=': '$eq',
+        '<': '$lt',
+        '<=': '$lte',
+      };
+      const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+      let filters = numericFilters.replace(
+        regEx,
+        (match) => `-${operatorMap[match]}-`
+      );
+      const options = ['price', 'rating'];
+      filters = filters.split(',').forEach((item) => {
+        const [field, operator, value] = item.split('-');
+        if (options.includes(field)) {
+          queryObject[field] = { [operator]: Number(value) };
+        }
+      });
+    }
+  
+    let result = Product.find(queryObject);
+    if (sort) {
+      const sortList = sort.split(',').join(' ');
+      result = result.sort(sortList);
+    } else {
+      result = result.sort('createdAt');
+    }
+  
+    if (fields) {
+      const fieldsList = fields.split(',').join(' ');
+      result = result.select(fieldsList);
+    }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+  
+    result = result.skip(skip).limit(limit);
 
-    console.log(queryObject)
-    const products = await Product.find(req.query)
+  
+    const products = await result;
     res.status(200).json({ products, nbHits: products.length });
-};
-
-module.exports = {
+  };
+  
+  module.exports = {
     getAllProducts,
     getAllProductsStatic,
-}
+  };
